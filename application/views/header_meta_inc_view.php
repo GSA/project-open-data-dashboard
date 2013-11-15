66a315cb8cc0797c525590502668d9b20ab47bf6
Philip Ashlock <phil@civicagency.org>
2013-11-15 16:24:32 -0500

    favicon href

--- application/views/header_meta_inc_view.php
+++ application/views/header_meta_inc_view.php
@@ -21,3 +21,4 @@
         <link rel="stylesheet" href="/css/main.css">
 
         <script src="/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <link rel="shortcut icon" href="/favicon.ico" />
