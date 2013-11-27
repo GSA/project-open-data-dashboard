d1d257462620a251ede2ffa5ca75403fdf66c613
Philip Ashlock <phil@civicagency.org>
2013-11-27 16:40:46 -0500

    Force refresh URLs so they don't get a cached copy

--- application/helpers/api_helper.php
+++ application/helpers/api_helper.php
@@ -4,6 +4,7 @@ function curl_from_json($url, $array=false) {
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	$data=curl_exec($ch);
@@ -23,6 +24,7 @@ function curl_header($url) {
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_MAXREDIRS, 10);    
     curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($c, CURLOPT_FRESH_CONNECT, 1);    
     curl_setopt($ch, CURLOPT_FILETIME, true);
     curl_setopt($ch, CURLOPT_NOBODY, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
