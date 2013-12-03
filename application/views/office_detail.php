6c31bf7dfe657143a841b108576320412568f2d1
Philip Ashlock <phil@civicagency.org>
2013-12-03 18:58:07 -0500

    Views for digital strategy json

--- application/views/office_detail.php
+++ application/views/office_detail.php
@@ -43,7 +43,11 @@
 			
 			if(!empty($office_campaign->datapage_status)) {
 				$office_campaign->datapage_status = json_decode($office_campaign->datapage_status);			
-			}			
+			}		
+			
+			if(!empty($office_campaign->digitalstrategy_status)) {
+				$office_campaign->digitalstrategy_status = json_decode($office_campaign->digitalstrategy_status);			
+			}				
 		?>
 		
 		
@@ -314,6 +318,191 @@
         </div>
         <?php endif; ?>	
                 
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+		<?php if(!empty($office_campaign->digitalstrategy_status)): ?>
+
+    	<div class="panel panel-default">
+    	<div class="panel-heading">/digitalstrategy.json</div>
+
+    	<table class="table table-striped table-hover">		
+
+    	<tr>
+    		<th>Expected /digitalstrategy.json URL</th>
+    		<td>
+    			<?php if(!empty($office_campaign->digitalstrategy_status->expected_url)): ?>
+    				<a href="<?php echo $office_campaign->digitalstrategy_status->expected_url ?>"><?php echo $office_campaign->digitalstrategy_status->expected_url ?></a>
+    		        <span style="color:#ccc"> (From <a style="color:#ccc; text-decoration:underline" href="http://www.usa.gov/About/developer-resources/federal-agency-directory/">USA.gov Directory</a>)</span>			
+    			<?php endif; ?>
+
+    			<?php 
+
+    				$http_code = (!empty($office_campaign->digitalstrategy_status->http_code)) ? $office_campaign->digitalstrategy_status->http_code : 0;
+
+    				switch ($http_code) {
+    				    case 404:
+    				        $status_color = 'danger';
+    				        break;
+    				    case 200:
+    				        $status_color = 'success';
+    				        break;
+    				    case 0:
+    				        $status_color = '';
+    				        break;					
+    				    default:
+    						$status_color = 'warning';
+    				}	
+
+    				if(!empty($office_campaign->digitalstrategy_status->content_type)) {
+    					if (strpos($office_campaign->digitalstrategy_status->content_type, 'application/json') !== false) {
+    						$mime_color = 'success';
+    					} else {
+    						$mime_color = 'danger';
+    					}						
+    				} else {
+    						$mime_color = 'danger';
+    				}
+
+    			?>
+
+    		</td>
+    	</tr>
+
+
+
+        <tr>
+        	<th>Resolved /digitalstrategy.json URL</th>
+        	<td>
+        		<a href="<?php echo $office_campaign->digitalstrategy_status->url ?>"><?php echo $office_campaign->digitalstrategy_status->url ?></a>
+        	</td>
+        </tr>	
+
+        <tr>
+        	<th>Redirects</th>
+        	<td>
+        		<?php if(!empty($office_campaign->digitalstrategy_status->redirect_count)): ?>
+        		<span class="text-<?php echo ($office_campaign->digitalstrategy_status->redirect_count > 5) ? 'danger' : 'warning'?>">
+        			<?php echo $office_campaign->digitalstrategy_status->redirect_count . ' redirects'; ?>
+        		</span>	
+            		<?php if($office_campaign->digitalstrategy_status->redirect_count > 5): ?>			
+            		    <span style="color:#ccc"> (stops tracking after 6)</span>
+            		<?php endif; ?>			        		
+        		<?php endif; ?>			
+        	</td>
+        </tr>		
+
+
+        <tr class="<?php echo $status_color;?>">
+        	<th>HTTP Status</th>
+        	<td>
+        		<span class="text-<?php echo $status_color;?>">
+        			<?php echo $office_campaign->digitalstrategy_status->http_code?>
+        		</span>			
+        	</td>
+        </tr>				
+
+        <tr class="<?php echo $mime_color;?>">
+        	<th>Content Type</th>
+        	<td>
+        		<span class="text-<?php echo $mime_color;?>">
+        			<?php echo $office_campaign->digitalstrategy_status->content_type?>
+        		</span>			
+        	</td>
+        </tr>	
+        
+        <?php if($http_code == 200 && $digital_strategy = curl_from_json($office_campaign->digitalstrategy_status->url, false, true)) {
+                $valid_json = true;
+              } else {
+                $valid_json = false;
+              }
+        ?>
+        
+        
+        
+		<tr class="<?php echo ($valid_json == true) ? 'success' : 'danger'?>">
+			<th>Valid JSON</th>
+			<td>
+			<span class="text-<?php echo ($valid_json == true) ? 'success' : 'danger'?>">
+			<?php		
+				if($valid_json == true) echo 'Valid';
+				if(($valid_json == false && $valid_json !== null) || ($office_campaign->digitalstrategy_status->http_code == 200 && $valid_json != true)) echo 'Invalid <span><a href="http://jsonlint.com/">Check a JSON Validator</a></span>';			
+			?>
+			</td>
+		</tr>        	
+
+        </table>
+        </div>
+        <?php endif; ?>
+                 
+                
+         <?php if($valid_json == true && !empty($digital_strategy)): ?>
+
+     	    <div class="panel panel-default">
+     	    <div class="panel-heading">Digital Strategy</div>
+     	    <div style="padding : 1em;">
+            <?php 
+                $sections = array("1.2.4", "1.2.5", "1.2.6", "1.2.7");
+            
+                foreach ($digital_strategy->items as $item) {
+                    if (in_array($item->id, $sections)) {
+                        echo "<h3>{$item->id} {$item->text}</h3>";
+                        
+                        if($item->multiple === false) {
+                            echo "<h4>{$item->fields[0]->label}</h4>";
+                            echo '<br>';
+                            echo $item->fields[0]->value ;                           
+                        } else {
+                            
+                            $columns = count($item->fields);
+                            $rows   = count($item->fields[0]->value);
+                            
+                            
+
+                            for ($row=0; $row < $rows; $row++) {
+                                
+                                echo '<table class="table table-striped table-hover" style="margin-bottom : 4em; border-bottom : 3px solid #ccc">';
+                                
+                                for ($column=0; $column< $columns; $column++) {
+                                    echo '<tr>';
+                                    echo '<th class="col-sm-2 col-md-2 col-lg-2">' . "{$item->fields[$column]->label}</th>";
+                                    
+                                    echo '<td class="col-sm-10 col-md-10 col-lg-10">';
+                                    if(!empty($item->fields[$column]->value[$row])) {
+                                        echo $item->fields[$column]->value[$row];
+                                    }
+                                    echo "</td>";
+                                    
+                                    echo "</tr>";                                    
+                                }
+                                
+                                echo '</table>';                                 
+
+                            }
+                            
+                        }
+                        
+                        echo '<hr>';
+                                            
+                    }
+                }
+            
+             ?>
+             </div>
+             </div>
+ 		
+ 		<?php endif;?>       
+                
+                
+
         <?php endif; ?>	
 
 
