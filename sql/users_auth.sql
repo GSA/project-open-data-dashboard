0686a2d1c671a9749317370d66450250d41ed8b6
Philip Ashlock <phil@civicagency.org>
2014-04-30 19:33:46 -0400

    sql to create users table

---
+++ sql/users_auth.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.4
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 30, 2014 at 07:33 PM
+-- Server version: 5.5.28
+-- PHP Version: 5.3.26
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `farm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_auth`
+--
+
+CREATE TABLE IF NOT EXISTS `users_auth` (
+  `user_id` int(8) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `username_url` varchar(255) NOT NULL,
+  `name_full` varchar(255) DEFAULT NULL,
+  `provider_url` text NOT NULL,
+  `provider_user_id` int(12) NOT NULL,
+  `token` text NOT NULL,
+  `provider` varchar(255) NOT NULL DEFAULT 'github',
+  `permissions` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
