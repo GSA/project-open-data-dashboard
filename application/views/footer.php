c533d96324e863fdedaaa43b49178d94df2dfd4e
Philip Ashlock <phil@civicagency.org>
2014-07-18 11:11:26 -0400

    Remove old ga tracking code script

--- application/views/footer.php
+++ application/views/footer.php
@@ -27,18 +27,5 @@
 
         <script src="<?php echo site_url('js/main.js')?>"></script>
 
-
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', '<?php if($this->config->item('google_analytics_id')) echo $this->config->item('google_analytics_id') ?>', '<?php if($this->config->item('google_analytics_domain')) echo $this->config->item('google_analytics_domain') ?>');
-          ga('send', 'pageview');
-
-        </script>
-
-
     </body>
 </html>
