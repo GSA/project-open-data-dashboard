e33598344beea0a4426eada821f1f6bdefc8908c
Dave Stevenson <david.stevenson@tcg.com>
2015-09-30 14:27:36 -0400

    IEEUIT-1911 #comment Removed old Google Analytics code, and enabled use of Google Analytics account ID in DAP code.

--- application/views/header_inc_view.php
+++ application/views/header_inc_view.php
@@ -4,22 +4,8 @@
         <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
 
-<?php if(($this->config->item('google_analytics_id'))): ?>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', '<?php echo $this->config->item('google_analytics_id')?>', 'auto');
-    ga('set', 'anonymizeIp', true);
-    ga('send', 'pageview');
-
-  </script>
-<?php endif; ?>
-
 <?php if($this->config->item('enable_analytics')): ?>
-  <script id="_fed_an_ua_tag" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=OMB"></script>
+  <script id="_fed_an_ua_tag" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=OMB<?php print $this->config->item('google_analytics_id') ? '&pua=' . $this->config->item('google_analytics_id') : ''; ?>"></script>
 <?php endif; ?>
 
   <div class="skipnav">
