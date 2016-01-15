d9ef3e272438c4dc71bbb60c882de95d9da0f35e
Diego Ruiz <diego.ruiz@reisystems.com>
2016-01-15 12:17:24 -0500

    IEEUIT-2342 #comment commenting out GAO recommendations, keeping updates to minimum for easy security audit

--- application/views/office_list.php
+++ application/views/office_list.php
@@ -68,7 +68,7 @@ if($show_all_fields) {
                 status_table('Other Independent Offices', $independent_offices, $config, $milestone->selected_milestone, $milestone->specified);
             }
 
-            status_table_gao($this, $milestone);
+            //status_table_gao($this, $milestone);
 
             ?>
 
