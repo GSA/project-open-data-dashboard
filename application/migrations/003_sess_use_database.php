4db7373c2fdbe2ddadfd8c77cf853627ce24ad7d
Alex Perfilov <alexandr.perfilov@reisystems.com>
2016-08-16 14:09:34 -0400

    sessions to db

---
+++ application/migrations/003_sess_use_database.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Sess_use_database extends CI_Migration
+{
+
+    public function up()
+    {
+        $this->db->query("CREATE TABLE IF NOT EXISTS  `ci_sessions` (
+            session_id varchar(40) DEFAULT '0' NOT NULL,
+            ip_address varchar(45) DEFAULT '0' NOT NULL,
+            user_agent varchar(120) NOT NULL,
+            last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+            user_data text NOT NULL,
+            PRIMARY KEY (session_id),
+            KEY `last_activity_idx` (`last_activity`)
+        );");
+    }
+
+}
+
+
+
