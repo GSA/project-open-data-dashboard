41956c29f41b882bfef6637baa862a1f55face72
Alex Perfilov <alexandr.perfilov@reisystems.com>
2017-09-14 23:24:18 -0400

    Better escaping output against XSS

--- application/views/changeset_result.php
+++ application/views/changeset_result.php
@@ -7,19 +7,19 @@
 
       <div class="row">
         <div class="col-lg-12">
-          <h2 style="margin-bottom : 0; padding-bottom : 0">Changeset Report for <?php echo $datajson_domain; ?></h2>
+          <h2 style="margin-bottom : 0; padding-bottom : 0">Changeset Report for <?php echo htmlentities($datajson_domain); ?></h2>
           <div style="color: #666">date: <?php echo date('l, F j Y g:i a')?></div>
 
 
 <div style="margin-bottom : 2em; border-bottom : 1px solid #ccc">
 
-    <h3 style="color : #666; margin-bottom : 0; padding-bottom : 0">Data listings in catalog.data.gov: <span style="color : #000"><?php echo $old_count ?></span></h3>
-    <div style="color: #666">source: <a style="color: #666" href="<?php echo $json_old_url; ?>"><?php echo $json_old_request; ?></a></div>
+    <h3 style="color : #666; margin-bottom : 0; padding-bottom : 0">Data listings in catalog.data.gov: <span style="color : #000"><?php echo ($old_count + 0) ?></span></h3>
+    <div style="color: #666">source: <a style="color: #666" href="<?php echo htmlentities($json_old_url); ?>"><?php echo htmlentities($json_old_request); ?></a></div>
 
-    <h3 style="color : #666; margin-bottom : 0; padding-bottom : 0">Data listings in data.json: <span style="color : #000"><?php echo $new_count ?></span></h3>
-    <div style="color: #666">source: <a style="color: #666" href="<?php echo $datajson_new_url ?>"><?php echo $datajson_new_url; ?></a></div>
+    <h3 style="color : #666; margin-bottom : 0; padding-bottom : 0">Data listings in data.json: <span style="color : #000"><?php echo ($new_count + 0) ?></span></h3>
+    <div style="color: #666">source: <a style="color: #666" href="<?php echo htmlentities($datajson_new_url) ?>"><?php echo htmlentities($datajson_new_url); ?></a></div>
 
-    <h3 style="color : blue">Matches: <?php echo $match_count ?></h3>
+    <h3 style="color : blue">Matches: <?php echo ($match_count + 0) ?></h3>
 
     <h3 style="color : red">Removed: <?php echo ($old_count - $match_count) ?></h3>
     <h3 style="color : green">Added: <?php echo ($new_count - $match_count) ?></h3>
@@ -35,10 +35,10 @@ if(!empty($changeset)) {
 ?>
     <?php if ($change['match']): ?>
         <div style="margin : 2em 0 2em 0">
-            <div style="color: green">Matches found for <a href="<?php echo $change['url'] ?>"><?php echo $change['url'] ?></a></div>
+            <div style="color: green">Matches found for <a href="<?php echo htmlentities($change['url']) ?>"><?php echo htmlentities($change['url']) ?></a></div>
 
             <?php foreach ($change['matches'] as $match) { ?>
-                <div><?php echo $match; ?></div>
+                <div><?php echo htmlentities($match); ?></div>
             <?php } ?>
 
         </div>
@@ -47,7 +47,7 @@ if(!empty($changeset)) {
 
 
     <?php if (!$change['match']): ?>
-        <div style="color: red">No match found for <a href="<?php echo $change['url'] ?>"><?php echo $change['url'] ?></a></div>
+        <div style="color: red">No match found for <a href="<?php echo htmlentities($change['url']) ?>"><?php echo htmlentities($change['url']) ?></a></div>
     <?php endif;  ?>
 
 
