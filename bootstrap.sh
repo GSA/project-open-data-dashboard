7b7e7d91b1a837d8655fdf384b3e9bfc025dee2b
Peter Burkholder <peter.burkholder@gsa.gov>
2019-08-26 13:03:30 -0400

    Coalesing around docker and cf
    Using VCAP_SERVICES as authoritative for docker and cloud foundry.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,15 @@ fail() {
   exit 1
 }
 
-if [ ! -z "${VCAP_SERVICES:-''}" ]; then 
-  SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "secrets") | .credentials') ||
-    fail "Unable to parse SECRETS from VCAP_SERVICES"
-  ENCRYPTION_KEY=$(echo $SECRETS | jq -r '.ENCRYPTION_KEY')
+SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "secrets") | .credentials') ||
+  fail "Unable to parse SECRETS from VCAP_SERVICES"
+ENCRYPTION_KEY=$(echo $SECRETS | jq -r '.ENCRYPTION_KEY')
 
-  DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
-  DB_USER=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.username')
-  DB_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.password')
-  DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
-  DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
-fi
+DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
+DB_USER=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.username')
+DB_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.password')
+DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
+DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
 
 :>.env
 for e in DB_NAME DB_USER DB_PASSWORD DB_HOST DB_PORT ENCRYPTION_KEY; do 
