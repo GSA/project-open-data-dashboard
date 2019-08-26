47fa3ad3367c726e79a1d2a53978fba508ef0149
Peter Burkholder <peter.burkholder@gsa.gov>
2019-08-26 11:11:32 -0400

    Changes for cloud.gov
    Added `jq` to docker build
    
    docker-compose using DB_PORT and entrypoint of startup.sh
    
    startup.sh and wait_for_db cribbed from Drupal examples
    
    bootstrap.sh should be able to parse VCAP_SERVICES

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,23 +6,25 @@ fail() {
   exit 1
 }
 
-SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "secrets") | .credentials') ||
-  fail "Unable to parse SECRETS from VCAP_SERVICES"
+if [ ! -z ${VCAP_SERVICES:-''} ]; then 
+  SECRETS=$(echo $VCAP_SERVICES | jq -r '.["user-provided"][] | select(.name == "secrets") | .credentials') ||
+    fail "Unable to parse SECRETS from VCAP_SERVICES"
+  ENCRYPTION_KEY=$(echo $SECRETS | jq -r '.ENCRYPTION_KEY')
 
-DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
-DB_USER=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.username')
-DB_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.password')
-DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
-DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
-
-ENCRYPTION_KEY=$(echo $SECRETS | jq -r '.ENCRYPTION_KEY')
+  DB_NAME=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.db_name')
+  DB_USER=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.username')
+  DB_PASSWORD=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.password')
+  DB_HOST=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.host')
+  DB_PORT=$(echo $VCAP_SERVICES | jq -r '.["aws-rds"][] | .credentials.port')
+fi
 
 :>.env
-for e in DB_NAME DB_USER DB_PASSWORD DB_HOST DB_PASSWORD ENCRYPTION_KEY; do 
+for e in DB_NAME DB_USER DB_PASSWORD DB_HOST DB_PORT ENCRYPTION_KEY; do 
   echo "$e: ${!e}" >> .env
 done 
 
-exit 
+exec "apache2-foreground"
+
 #APP_NAME=$(echo $VCAP_APPLICATION | jq -r '.name') ||
 #  fail "Unable to parse APP_NAME from VCAP_SERVICES"
 #APP_ROOT=$(dirname "${BASH_SOURCE[0]}")
