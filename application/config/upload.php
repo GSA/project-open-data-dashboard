56afe5d1111e734635390199b28a841e8cc4b1a6
Peter Burkholder <peter.burkholder@gsa.gov>
2019-09-20 13:00:50 -0400

    Use PROJECT_SHARED_PATH env var in upload config
    Seems that `upload.php` doesn't have access to the config
    values set in `config.php` so we need to use values
    from env vars. This will get overwritten in the
    Ansible envionments, but suffices for testing in docker-compose
    and later for work in cloud.gov

--- application/config/upload.php
+++ application/config/upload.php
@@ -6,7 +6,11 @@
 |--------------------------------------------------------------------------
 */
 
-$project_shared_path = $config['project_shared_path'];
+// This following line doesn't work and I (peterb) don't know why
+//   $project_shared_path = $config['project_shared_path'];
+// so using this:
+$project_shared_path = getenv("PROJECT_SHARED_PATH") ?: "/var/www/app";
+
 $config['upload_path'] = $project_shared_path . '/uploads/'; // absolute path
 $config['allowed_types'] = '*'; //'gif|jpg|png|csv|txt|JPG|GIF|PNG|CSV|TXT';
 $config['max_size'] = '500000';
