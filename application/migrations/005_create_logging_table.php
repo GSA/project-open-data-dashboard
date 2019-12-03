f4548a666237d6d8d5e049b22755cc5a3eb152c2
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2019-12-02 21:41:40 -0800

    Refactor so existing tables will also drop the NOT NULL constraint

--- application/migrations/005_create_logging_table.php
+++ application/migrations/005_create_logging_table.php
@@ -13,10 +13,10 @@ class Migration_create_logging_table extends CI_Migration
             ip                      VARCHAR(10) NOT NULL,
             page                    VARCHAR(255) NOT NULL,
             user_agent              VARCHAR(255) NOT NULL,
-            referrer                VARCHAR(255) default NULL,
+            referrer                VARCHAR(255) NOT NULL,
             logged                  TIMESTAMP NOT NULL
                                     default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-            username                VARCHAR(255) default NULL,
+            username                VARCHAR(255) NOT NULL,
             memory                  INT UNSIGNED NOT NULL,
             render_elapsed          FLOAT NOT NULL,
             ci_elapsed              FLOAT NOT NULL,
@@ -26,6 +26,8 @@ class Migration_create_logging_table extends CI_Migration
             mysql_queries           TEXT NOT NULL
         ) ENGINE=ARCHIVE;
         ');
+        $this->db->query('ALTER TABLE ci_logs CHANGE referrer referrer VARCHAR(255) DEFAULT NULL;');
+        $this->db->query('ALTER TABLE ci_logs CHANGE username username VARCHAR(255) DEFAULT NULL;');
 
 
     }
