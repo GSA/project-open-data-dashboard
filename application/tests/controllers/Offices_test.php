f7c024e55de21d36cd031b0be7bd12424fd512c2
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-01-21 11:49:12 -0800

    Correct syntax error introduced by resolving conflict in GitHub

--- application/tests/controllers/Offices_test.php
+++ application/tests/controllers/Offices_test.php
@@ -8,8 +8,9 @@ class OfficesTest extends TestCase
         // but at least in our dev/test environments, no crawls have run yet, so the DB should be empty.
         $this->request('GET', 'offices/detail/49015');
         $this->assertResponseCode(200);
+    }
 
-  /**
+    /**
      * These are strapping tests that just assert that no PHP errors are encountered on clicks of nav links
      * @dataProvider badMilestoneProvider
      */
