123e951b12a106aa07f57e46ad561f223c805c2e
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2020-06-07 21:33:17 -0700

    Remove CSV to JSON conversion functionality

---
+++ application/views/csv_upload_removed.php
@@ -0,0 +1,14 @@
+<?php include 'header_meta_inc_view.php';?>
+
+<?php include 'header_inc_view.php';?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1>CSV conversion is no longer supported</h1>
+            <p>If you’ve used the CSV to JSON tool in the past, please
+            <a href="mailto:datagov@gsa.gov">contact us</a>; we’d love to hear from you.</p>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
