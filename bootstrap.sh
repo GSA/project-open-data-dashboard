ff1b45b261181e51e28e50ef25ed33dfd521a4d6
Chris MacDermaid <christopher.macdermaid@gsa.gov>
2021-03-19 15:12:44 -0600

    Add New Relic monitoring

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,16 @@ fail() {
   exit 1
 }
 
+function vcap_get_service () {
+  local path name
+  name="$1"
+  path="$2"
+  service_name=${APP_NAME}-${name}
+  echo $VCAP_SERVICES | jq --raw-output --arg service_name "$service_name" ".[][] | select(.name == \$service_name) | $path"
+}
+
+export NEW_RELIC_LICENSE_KEY=$(vcap_get_service secrets .credentials.NEW_RELIC_LICENSE_KEY)
+
 mkdir -p $APP_DIR/uploads
 chmod 777 $APP_DIR/uploads
 
