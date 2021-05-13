29dfae40416bc6a3f73519325f75b736e7a8e1dd
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-05-12 17:36:45 -0700

    Fix spacing

--- application/helpers/api_helper.php
+++ application/helpers/api_helper.php
@@ -7,7 +7,7 @@ function curl_from_json($url, $array=false, $decode=true) {
 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 
     curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
     curl_setopt($ch, CURLOPT_FILETIME, true);
