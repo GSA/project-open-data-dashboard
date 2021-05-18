b667692157109b2ea4887afac365442cee6d3dc6
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-05-17 21:24:49 -0700

    Add FCC to the list of OMB-monitored agencies
    Per https://github.com/GSA/datagov-deploy/issues/3206
    
    (cherry picked from commit 46d14ef35b974b6ea6c98b8ccdae28ed296b8a12)

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 12;
+$config['migration_version'] = 13;
 
 /*
 |--------------------------------------------------------------------------
