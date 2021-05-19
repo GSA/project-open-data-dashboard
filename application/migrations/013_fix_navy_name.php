1ae44dc1e4e187e990e0cb18a7a7dc56c5da7e71
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-05-19 13:56:24 -0700

    Bring Navy name update change from FCS to main
    updates "Navy" to "U.S. Navy"
    
    This change is migration 13 on main, while it was migration 12 on fcs (because it was never migrated back, and meanwhile main got a different migration 12).

---
+++ application/migrations/013_fix_navy_name.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_fix_navy_name extends CI_Migration
+{
+
+    public function up()
+    {
+        // Update Navy's name
+        $this->db->query('UPDATE offices SET name = "U.S. Navy" WHERE id = "49561"');
+    }
+
+    public function down()
+    {
+      $this->db->query('UPDATE offices SET name = "Navy" WHERE id = "49561"');
+    }
+}
