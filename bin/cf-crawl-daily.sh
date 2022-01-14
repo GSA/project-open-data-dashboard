8825080de3a586681da7ebd39fbcf0e95af7aac9
James Brown <james.c.brown@gsa.gov>
2022-01-14 08:52:55 -0700

    Remove --wait until cf8 bug is fixed

--- bin/cf-crawl-daily.sh
+++ bin/cf-crawl-daily.sh
@@ -4,6 +4,6 @@ set -e
 
 # To be executed with the app to use, ie ./cf-crawl-daily.sh dashboard-stage
 
-cf run-task "$1" --command "php public/index.php campaign status omb-monitored download" --wait --name dashboard-omb-monitored-download
+cf run-task "$1" --command "php public/index.php campaign status omb-monitored download" --name dashboard-omb-monitored-download
 
-cf run-task "$1" --command "php public/index.php campaign status omb-monitored full-scan" --wait --name dashboard-omb-monitored-full-scan
+cf run-task "$1" --command "php public/index.php campaign status omb-monitored full-scan" --name dashboard-omb-monitored-full-scan
